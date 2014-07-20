6bbc86bbefb1f94af3455b13808291e50a051b01
Zhao <z63zhao@uwaterloo.ca>
2014-07-20 09:27:13 -0400

    comments
    lalala

--- StraightGUI/CardButton.cc
+++ StraightGUI/CardButton.cc
@@ -6,24 +6,27 @@
 //  Copyright (c) 2014 Jack,Errin. All rights reserved.
 //
 
-#include <gtkmm.h>
-#include <iostream>
 #include "CardButton.h"
 
+//constructors, set up the rank and suit for a button
 CardButton::CardButton(const int& r, const int& s): rank_(r), suit_(s){}
 
+//accessor to get which card was played
 int CardButton::getRank(){
     return rank_;
 }
 
+//accessor to get which card was played
 int CardButton::getSuit(){
     return suit_;
 }
 
+//mutator for update()
 void CardButton::setSuit(int suit) {
     suit_ = suit;
 }
 
+//mutator for update()
 void CardButton::setRank(int rank){
     rank_ = rank;
 }
